3be901b2635f65f0e0ead6beae754dfd60df0fec
Philip Meier <pmeier@8thlight.com>
2017-12-01 17:46:50 +0000

    Day 1 Part 2

--- day1/test/LibSpec.hs
+++ day1/test/LibSpec.hs
@@ -11,11 +11,13 @@ main = hspec spec
 spec :: Spec
 spec =
   describe "captcha" $ do
-    it "returns 3 for 1122" $ do
-      captcha "1122" `shouldBe` 3
-    it "returns 4 for 1111" $ do
-      captcha "1111" `shouldBe` 4
-    it "returns 0 for 1234" $ do
-      captcha "1234" `shouldBe` 0
-    it "returns 9 for 91212129" $ do
-      captcha "91212129" `shouldBe` 9
+    it "returns 6 for 1212" $ do
+      captcha "1212" `shouldBe` 6
+    it "returns 0 for 1221" $ do
+      captcha "1221" `shouldBe` 0
+    it "returns 4 for 123425" $ do
+      captcha "123425" `shouldBe` 4
+    it "returns 12 for 123123" $ do
+      captcha "123123" `shouldBe` 12
+    it "returns 4 for 12131415" $ do
+      captcha "12131415" `shouldBe` 4
